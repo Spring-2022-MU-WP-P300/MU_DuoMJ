2d3bbaf4a64a8cc59741110ea51a06f0cccdcb28
Hyperactive-knucklehead <htcrayhan2015@outlook.com>
2022-05-22 21:43:12 +0600

    post an event (CREATE event)

--- Volunteer Network/volunteer-network-server/server.js
+++ Volunteer Network/volunteer-network-server/server.js
@@ -22,6 +22,15 @@ async function run() {
     try {
       await client.connect();
       const database = client.db("volunteer_network");
+      const eventCollection = database.collection("events");
+
+      //post an event
+        app.post("/events", async (req, res) => {
+        const event = req.body;
+        const result = await eventCollection.insertOne(event);
+        res.send(result);
+      });
+      
     } finally {
       // await client.close();
     }
