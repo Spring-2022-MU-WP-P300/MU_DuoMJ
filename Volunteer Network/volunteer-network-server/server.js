16c7d1d3bab9c152b76ad2dfb941544d5e4340f4
Hyperactive-knucklehead <htcrayhan2015@outlook.com>
2022-05-22 21:51:58 +0600

    get events (READ events)

--- Volunteer Network/volunteer-network-server/server.js
+++ Volunteer Network/volunteer-network-server/server.js
@@ -1,8 +1,8 @@
-const express= require('express');
+const express = require("express");
 const app = express();
-const cors = require('cors');
-const {MongoClient} = require('mongodb');
-const {ObjectID} = require('mongodb');
+const cors = require("cors");
+const { MongoClient } = require("mongodb");
+const { ObjectID } = require("mongodb");
 const port = process.env.PORT || 5000;
 require("dotenv").config();
 
@@ -10,7 +10,7 @@ require("dotenv").config();
 app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
-app.get("/",(req,res)=>res.send("Volunteer Network Server is running"));
+app.get("/", (req, res) => res.send("Volunteer Network Server is running"));
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bm2i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, {
@@ -19,22 +19,26 @@ const client = new MongoClient(uri, {
 });
 
 async function run() {
-    try {
-      await client.connect();
-      const database = client.db("volunteer_network");
-      const eventCollection = database.collection("events");
+  try {
+    await client.connect();
+    const database = client.db("volunteer_network");
+    const eventCollection = database.collection("events");
 
-      //post an event
-        app.post("/events", async (req, res) => {
+    //post an event
+    app
+      .post("/events", async (req, res) => {
         const event = req.body;
         const result = await eventCollection.insertOne(event);
         res.send(result);
+      })
+      .get("/events", async (req, res) => {
+        const events = await eventCollection.find().toArray();
+        res.send(events);
       });
-      
-    } finally {
-      // await client.close();
-    }
+  } finally {
+    // await client.close();
   }
-  run().catch(console.dir);
+}
+run().catch(console.dir);
 
-app.listen(port,()=>console.log(`Server is running on port ${port}`));
+app.listen(port, () => console.log(`Server is running on port ${port}`));
