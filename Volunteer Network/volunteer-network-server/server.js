5bbc1ee11d722c00856780d94ca2485ce5eccee2
Hyperactive-knucklehead <htcrayhan2015@outlook.com>
2022-05-22 21:30:51 +0600

    Integrated with mongodb database

--- Volunteer Network/volunteer-network-server/server.js
+++ Volunteer Network/volunteer-network-server/server.js
@@ -11,4 +11,21 @@ app.use(cors());
 app.use(express.json({ limit: "50mb" }));
 
 app.get("/",(req,res)=>res.send("Volunteer Network Server is running"));
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.bm2i5.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
+const client = new MongoClient(uri, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+async function run() {
+    try {
+      await client.connect();
+      const database = client.db("volunteer_network");
+    } finally {
+      // await client.close();
+    }
+  }
+  run().catch(console.dir);
+
 app.listen(port,()=>console.log(`Server is running on port ${port}`));
